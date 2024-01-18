7f7425fc36d36d261ea02e72b6e9c1fb944e0063
goodnewsakpan <goodnewsakpan123@gmail.com>
2024-01-18 18:30:42 +0100

    doubles function

--- double_functions.c
+++ double_functions.c
@@ -10,33 +10,33 @@
 
 stack_t *add_dnodeint_end(stack_t **head, const int n)
 {
-stack_t *temp, *aux;
+	stack_t *temp, *aux;
 
-if (head == NULL)
-return (NULL);
-temp = malloc(sizeof(stack_t));
-if (!temp)
-{
-dprintf(2, "Error: malloc failed\n");
-free_vglo();
-exit(EXIT_FAILURE);
-}
-temp->n = n;
+	if (head == NULL)
+		return (NULL);
+	temp = malloc(sizeof(stack_t));
+	if (!temp)
+	{
+		dprintf(2, "Error: malloc failed\n");
+		free_vglo();
+		exit(EXIT_FAILURE);
+	}
+	temp->n = n;
 
-if (*head == NULL)
-{
-temp->next = *head;
-temp->prev = NULL;
-*head = temp;
-return (*head);
-}
-aux = *head;
-while (aux->next)
-aux = aux->next;
-temp->next = aux->next;
-temp->prev = aux;
-aux->next = temp;
-return (aux->next);
+	if (*head == NULL)
+	{
+		temp->next = *head;
+		temp->prev = NULL;
+		*head = temp;
+		return (*head);
+	}
+	aux = *head;
+	while (aux->next)
+		aux = aux->next;
+	temp->next = aux->next;
+	temp->prev = aux;
+	aux->next = temp;
+	return (aux->next);
 }
 
 /**
@@ -49,31 +49,31 @@ return (aux->next);
 
 stack_t *add_dnodeint(stack_t **head, const int n)
 {
-stack_t *temp;
+	stack_t *temp;
 
-if (head == NULL)
-return (NULL);
-temp = malloc(sizeof(stack_t));
-if (!temp)
-{
-dprintf(2, "Error: malloc failed\n");
-free_vglo();
-exit(EXIT_FAILURE);
-}
-temp->n = n;
+	if (head == NULL)
+		return (NULL);
+	temp = malloc(sizeof(stack_t));
+	if (!temp)
+	{
+		dprintf(2, "Error: malloc failed\n");
+		free_vglo();
+		exit(EXIT_FAILURE);
+	}
+	temp->n = n;
 
-if (*head == NULL)
-{
-temp->next = *head;
-temp->prev = NULL;
-*head = temp;
-return (*head);
-}
-(*head)->prev = temp;
-temp->next = (*head);
-temp->prev = NULL;
-*head = temp;
-return (*head);
+	if (*head == NULL)
+	{
+		temp->next = *head;
+		temp->prev = NULL;
+		*head = temp;
+		return (*head);
+	}
+	(*head)->prev = temp;
+	temp->next = (*head);
+	temp->prev = NULL;
+	*head = temp;
+	return (*head);
 }
 
 /**
@@ -85,11 +85,11 @@ return (*head);
 
 void free_dlistint(stack_t *head)
 {
-stack_t *tmp;
+	stack_t *tmp;
 
-while ((tmp = head) != NULL)
-{
-head = head->next;
-free(tmp);
-}
+	while ((tmp = head) != NULL)
+	{
+		head = head->next;
+		free(tmp);
+	}
 }
