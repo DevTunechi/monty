745e14fb8122d42de5ac15e002f0e8ff46d54180
goodnewsakpan <goodnewsakpan123@gmail.com>
2024-01-18 18:33:53 +0100

    instruction opcode

--- instruction_opcode.c
+++ instruction_opcode.c
@@ -10,33 +10,33 @@
 
 void _push(stack_t **doubly, unsigned int cline)
 {
-int n, j;
+	int n, j;
 
-if (!vglo.arg)
-{
-dprintf(2, "L%u: ", cline);
-dprintf(2, "usage: push integer\n");
-free_vglo();
-exit(EXIT_FAILURE);
-}
+	if (!vglo.arg)
+	{
+		dprintf(2, "L%u: ", cline);
+		dprintf(2, "usage: push integer\n");
+		free_vglo();
+		exit(EXIT_FAILURE);
+	}
 
-for (j = 0; vglo.arg[j] != '\0'; j++)
-{
-if (!isdigit(vglo.arg[j]) && vglo.arg[j] != '-')
-{
-dprintf(2, "L%u: ", cline);
-dprintf(2, "usage: push integer\n");
-free_vglo();
-exit(EXIT_FAILURE);
-}
-}
+	for (j = 0; vglo.arg[j] != '\0'; j++)
+	{
+		if (!isdigit(vglo.arg[j]) && vglo.arg[j] != '-')
+		{
+			dprintf(2, "L%u: ", cline);
+			dprintf(2, "usage: push integer\n");
+			free_vglo();
+			exit(EXIT_FAILURE);
+		}
+	}
 
-n = atoi(vglo.arg);
+	n = atoi(vglo.arg);
 
-if (vglo.lifo == 1)
-add_dnodeint(doubly, n);
-else
-add_dnodeint_end(doubly, n);
+	if (vglo.lifo == 1)
+		add_dnodeint(doubly, n);
+	else
+		add_dnodeint_end(doubly, n);
 }
 
 /**
@@ -49,16 +49,16 @@ add_dnodeint_end(doubly, n);
 
 void _pall(stack_t **doubly, unsigned int cline)
 {
-stack_t *aux;
-(void)cline;
+	stack_t *aux;
+	(void)cline;
 
-aux = *doubly;
+	aux = *doubly;
 
-while (aux)
-{
-printf("%d\n", aux->n);
-aux = aux->next;
-}
+	while (aux)
+	{
+		printf("%d\n", aux->n);
+		aux = aux->next;
+	}
 }
 
 /**
@@ -71,17 +71,17 @@ aux = aux->next;
 
 void _pint(stack_t **doubly, unsigned int cline)
 {
-(void)cline;
+	(void)cline;
 
-if (*doubly == NULL)
-{
-dprintf(2, "L%u: ", cline);
-dprintf(2, "can't pint, stack empty\n");
-free_vglo();
-exit(EXIT_FAILURE);
-}
+	if (*doubly == NULL)
+	{
+		dprintf(2, "L%u: ", cline);
+		dprintf(2, "can't pint, stack empty\n");
+		free_vglo();
+		exit(EXIT_FAILURE);
+	}
 
-printf("%d\n", (*doubly)->n);
+	printf("%d\n", (*doubly)->n);
 }
 
 /**
@@ -94,17 +94,17 @@ printf("%d\n", (*doubly)->n);
 
 void _pop(stack_t **doubly, unsigned int cline)
 {
-stack_t *aux;
+	stack_t *aux;
 
-if (doubly == NULL || *doubly == NULL)
-{
-dprintf(2, "L%u: can't pop an empty stack\n", cline);
-free_vglo();
-exit(EXIT_FAILURE);
-}
-aux = *doubly;
-*doubly = (*doubly)->next;
-free(aux);
+	if (doubly == NULL || *doubly == NULL)
+	{
+		dprintf(2, "L%u: can't pop an empty stack\n", cline);
+		free_vglo();
+		exit(EXIT_FAILURE);
+	}
+	aux = *doubly;
+	*doubly = (*doubly)->next;
+	free(aux);
 }
 
 /**
@@ -117,24 +117,24 @@ free(aux);
 
 void _swap(stack_t **doubly, unsigned int cline)
 {
-int m = 0;
-stack_t *aux = NULL;
+	int m = 0;
+	stack_t *aux = NULL;
 
-aux = *doubly;
+	aux = *doubly;
 
-for (; aux != NULL; aux = aux->next, m++)
+	for (; aux != NULL; aux = aux->next, m++)
 		;
-if (m < 2)
-{
-dprintf(2, "L%u: can't swap, stack too short\n", cline);
-free_vglo();
-exit(EXIT_FAILURE);
-}
+	if (m < 2)
+	{
+		dprintf(2, "L%u: can't swap, stack too short\n", cline);
+		free_vglo();
+		exit(EXIT_FAILURE);
+	}
 
-aux = *doubly;
-*doubly = (*doubly)->next;
-aux->next = (*doubly)->next;
-aux->prev = *doubly;
-(*doubly)->next = aux;
-(*doubly)->prev = NULL;
+	aux = *doubly;
+	*doubly = (*doubly)->next;
+	aux->next = (*doubly)->next;
+	aux->prev = *doubly;
+	(*doubly)->next = aux;
+	(*doubly)->prev = NULL;
 }
