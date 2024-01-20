cd3f58823d3564ecdc895a7cbdc3647bd708f015
goodnewsakpan <goodnewsakpan123@gmail.com>
2024-01-20 23:32:24 +0100

    function that multiplies top two elements of stack

--- multiplication.c
+++ multiplication.c
@@ -19,7 +19,6 @@ while (h)
 h = h->next;
 len++;
 }
-
 if (len < 2)
 {
 fprintf(stderr, "L%d: can't mul, stack too short\n", counter);
@@ -28,7 +27,6 @@ free(bus.content);
 free_stack(*head);
 exit(EXIT_FAILURE);
 }
-
 h = *head;
 aux = h->next->n * h->n;
 h->next->n = aux;
