ec4c48932d7672ff566a30a824fee7cc128ca854
DevTunechi <tunjidare2@yahoo.com>
2024-01-20 04:08:41 -0800

    files adjusted

---
+++ main/stacks.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+
+/**
+ * f_stack - prints top stack
+ * @head: stack head
+ * Olatunji and Goodnews
+ * @counter: line num
+ * Return: nothing
+*/
+
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
